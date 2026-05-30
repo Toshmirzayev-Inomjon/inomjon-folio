2cfe046b7e584438947161d949d88c7594ba37e4
Inomjon <toshmirzayevinomjon@gmail.com>
2026-05-31 00:00:59 +0500

    prepare cpanel node deployment

---
+++ server.js
@@ -0,0 +1,17 @@
+const { createServer } = require("http");
+const next = require("next");
+
+const dev = process.env.NODE_ENV !== "production";
+const hostname = "0.0.0.0";
+const port = Number(process.env.PORT || process.env.NODE_PORT || 3000);
+
+const app = next({ dev, hostname, port });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  createServer((req, res) => {
+    handle(req, res);
+  }).listen(port, hostname, () => {
+    console.log(`Ready on http://${hostname}:${port}`);
+  });
+});
